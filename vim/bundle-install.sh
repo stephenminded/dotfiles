e886cc494840916b2de32a40049fb3673263dc01
stephenminded <steve.lounsbury@jadedpixel.com>
2014-01-21 16:56:22 +0000

    Update stuff

--- vim/bundle-install.sh
+++ vim/bundle-install.sh
@@ -17,7 +17,8 @@ refresh https://github.com/scrooloose/nerdtree.git      nerdtree
 refresh https://github.com/kchmck/vim-coffee-script.git vim-coffee-script
 refresh https://github.com/tpope/vim-vividchalk.git     vim-vividchalk
 refresh https://github.com/altercation/vim-colors-solarized.git vim-colors-solarized
-#refresh https://github.com/timcharper/textile.vim.git textile  
+refresh https://github.com/kien/ctrlp.vim.git ctrlp.vim
+#refresh https://github.com/timcharper/textile.vim.git textile
 # interesting https://github.com/Valloric/YouCompleteMe
 # https://github.com/garbas/vim-snipmate
 cd -
