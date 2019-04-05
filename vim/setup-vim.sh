c0da4f5699977320946bd79fb2b09a88bbbc1072
Steve Lounsbury <steve.lounsbury@shopify.com>
2019-04-05 10:43:16 -0400

    VIM changes:  - Switch to vim-plug  - Switch to vim-test  - Kill old plugin install system and trim plugin list  - Switch to fzf
    Random stuff
     - move over tweaks to zshrc, tmux and gitconfig

--- vim/setup-vim.sh
+++ vim/setup-vim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "This will destroy .vim, .vimrc and .gvimrc! Are you sure?"
+echo "This will destroy .vim, and .vimrc! Are you sure?"
 select yn in "Yes" "No"; do
   case $yn in
     Yes ) break;;
@@ -8,28 +8,14 @@ select yn in "Yes" "No"; do
 done
 
 rm -rf ~/.vim
-mkdir ~/.vim
+mkdir -p ~/.vim/autoload
 
-echo "Installing pathogen for VIM package management"
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Installing vim_plug for VIM package management"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Installing vimrc"
 echo "    copying vimrc to ~/.vimrc"
 cp vimrc ~/.vimrc
-
-echo "Installing gvimrc"
-echo "    copying gvimrc to ~/.gvimrc"
-cp gvimrc ~/.gvimrc
-
-echo "Installing bundle install script to ~/.vim/bundle"
-cp bundle-install.sh ~/.vim/bundle
-chmod +x ~/.vim/bundle/bundle-install.sh
-
-echo "Running bundle install..."
-cd ~/.vim/bundle
-./bundle-install.sh
-echo "    done."
-
 echo "Here's your .vim:"
 tree ~/.vim
