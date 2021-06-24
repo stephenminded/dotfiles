46b91c1b93b6ce9d83e06e07aab9be87cd1dc411
Steve Lounsbury <steve.lounsbury@shopify.com>
2021-06-23 23:41:25 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -fLo ~/.vim/colors/deus.vim --create-dirs https://raw.githubusercontent.com/ajmwagar/vim-deus/master/colors/deus.vim
 
-ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/dircolors.256dark ~/.dircolors.256dark
 
